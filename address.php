dae7a7f46cf7445c2e1e1f5668b625a280d99e68
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2023-12-24 10:29:01 +0800

    upload for users

---
+++ address.php
@@ -0,0 +1,11 @@
+﻿
+<?php
+// Create an associative array to be converted to JSON
+$response = array('lat' => '10.29652', 'log' =>'123.90741');
+
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Output the JSON response
+echo json_encode($response);
+?>
